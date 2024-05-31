70e2a6e392acfc696fa54676b0301d3e1f202e62
Dallas012 <socrativejh@gmail.com>
2024-06-01 00:21:42 +0900

    delete restaurant in api.py

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter
-from app.api.api_v1.endpoints import user, campus, restaurant, menu
+from app.api.api_v1.endpoints import user, campus, menu
 
 api_router = APIRouter()
 api_router.include_router(user.router, prefix="/users", tags=["users"])
 api_router.include_router(campus.router, prefix="/campuses", tags=["campuses"])
-api_router.include_router(restaurant.router, prefix="/restaurants", tags=["restaurants"])
 api_router.include_router(menu.router, prefix="/menus", tags=["menus"])

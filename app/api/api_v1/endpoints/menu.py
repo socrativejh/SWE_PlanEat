2c0d7b24ab028d55b0258f1a2f3df64031072a28
kanadae <kana_vi@naver.com>
2024-06-02 18:03:13 +0900

    tag로 메뉴 id 검색

--- app/api/api_v1/endpoints/menu.py
+++ app/api/api_v1/endpoints/menu.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Query
 from app.schemas.menu import Menu
 from app.crud import menu as crud_menu
 from typing import List
@@ -16,3 +16,10 @@ async def read_menu(menu_id: int):
     if not menu:
         raise HTTPException(status_code=404, detail="Menu not found")
     return Menu(id=menu["_id"], name=menu["name"], protein=menu["protein"], sugar=menu["sugar"], carbo=menu["carbo"], fat=menu["fat"], kcal=menu["kcal"], price=menu["price"], allergy=menu["allergy"], date=menu["date"], mealtime=menu["mealtime"], tags=menu["tags"])
+
+@router.get("/search_by_tags/", response_model=List[int])
+async def search_menu_ids_by_tags(tags: List[int] = Query(...)):
+    menu_ids = await crud_menu.get_menu_ids_by_tags(tags)
+    if not menu_ids:
+        raise HTTPException(status_code=404, detail="Menus not found")
+    return menu_ids
--- app/crud/menu.py
+++ app/crud/menu.py
@@ -1,4 +1,5 @@
 from app.db.mongodb import database
+from typing import List
 
 async def get_menu_by_id(menu_id: int):
     return await database.db['menus'].find_one({"_id": menu_id})
@@ -7,3 +8,9 @@ async def get_all_menus():
     menus_cursor = database.db['menus'].find()
     menus = await menus_cursor.to_list(length=None)
     return menus
+
+async def get_menu_ids_by_tags(tags: List[int]):
+    menus_cursor = database.db['menus'].find({"tags": {"$all": tags}}, {"_id": 1})
+    menus = await menus_cursor.to_list(length=None)
+    menu_ids = [menu["_id"] for menu in menus]
+    return menu_ids
